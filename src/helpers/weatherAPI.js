1a8bc1d6d7de39912d8a0d2812a1f56277028dfe
Izadora Braga <izadora_abraga@outlook.com>
2023-10-14 21:45:23 -0300

    feat: ajustes requisito 1

--- src/helpers/weatherAPI.js
+++ src/helpers/weatherAPI.js
@@ -2,8 +2,7 @@ const token = import.meta.env.VITE_TOKEN;
 
 export const searchCities = async (term) => {
   if (term) {
-    const response = await fetch(`
-    api.weatherapi.com/v1/search.json?lang=pt&key=${token}&q=${term}`);
+    const response = await fetch(`http://api.weatherapi.com/v1/search.json?lang=pt&key=${token}&q=${term}`);
     const data = response.json();
     return data;
   }
