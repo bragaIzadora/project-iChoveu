41a4f4ab08a574075fa23102171085190bfdffe7
Izadora Braga <izadora_abraga@outlook.com>
2023-10-14 21:36:15 -0300

    iniciando requisito 1 (teste)

--- src/helpers/weatherAPI.js
+++ src/helpers/weatherAPI.js
@@ -1,9 +1,18 @@
-// Remova os comentários a medida que for implementando as funções
+const token = import.meta.env.VITE_TOKEN;
 
-export const searchCities = (/* term */) => {
-//   seu código aqui
+export const searchCities = async (term) => {
+  if (term) {
+    const response = await fetch(`
+    api.weatherapi.com/v1/search.json?lang=pt&key=${token}&q=${term}`);
+    const data = response.json();
+    return data;
+  }
+  // if (data.length === 0) {
+  //   window.alert('Nenhuma cidade encontrada');
+  // }
+  //   seu código aqui
 };
 
 export const getWeatherByCity = (/* cityURL */) => {
-//   seu código aqui
+  //   seu código aqui
 };
