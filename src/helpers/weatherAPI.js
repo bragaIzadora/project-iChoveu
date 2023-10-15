53246878f21dbbe1c5d8b47e2a954cdd1071eb53
Izadora Braga <izadora_abraga@outlook.com>
2023-10-14 21:52:53 -0300

    feat: ajustes 2

--- src/helpers/weatherAPI.js
+++ src/helpers/weatherAPI.js
@@ -3,11 +3,15 @@ const token = import.meta.env.VITE_TOKEN;
 export const searchCities = async (term) => {
   if (term) {
     const response = await fetch(`http://api.weatherapi.com/v1/search.json?lang=pt&key=${token}&q=${term}`);
-    const data = response.json();
-    return data;
-  }
-  if (data.length === 0) {
-    window.alert('Nenhuma cidade encontrada');
+
+    if (response.ok) {
+      const data = await response.json();
+      if (data.length === 0) {
+        window.alert('Nenhuma cidade encontrada');
+      }
+
+      return data;
+    }
   }
 };
 
